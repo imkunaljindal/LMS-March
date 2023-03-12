4093f209887c920cc15a92eacb73d4f4f18dc85d
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-03-12 22:41:07 +0530

    Add Book, Author, Student and Card

---
+++ src/main/java/com/backendMarch/librarymanagementsystem/Entity/LibraryCard.java
@@ -0,0 +1,31 @@
+package com.backendMarch.librarymanagementsystem.Entity;
+
+import com.backendMarch.librarymanagementsystem.Enum.CardStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LibraryCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cardNo;
+
+    private String validTill;
+
+    @Enumerated(EnumType.STRING)
+    private CardStatus status;
+
+    @OneToOne
+    @JoinColumn
+    Student student;
+}
